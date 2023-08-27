78afc6c94f0bfb90c2d90ca411a180ee0f699206
Dmitry Skuratovich <dmitry.skuratovich@gmail.com>
2023-08-27 16:58:36 +0300

    Add Angular Material
    - add @angular/material dependency

--- ui/src/app/components/page-container/page-container.module.ts
+++ ui/src/app/components/page-container/page-container.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { PageContainerComponent } from './page-container.component';
 import { RouterLink } from '@angular/router';
 import { NgIf } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -13,6 +16,9 @@ import { NgIf } from '@angular/common';
   imports: [
     RouterLink,
     NgIf,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
   ],
 })
 export class PageContainerModule {
