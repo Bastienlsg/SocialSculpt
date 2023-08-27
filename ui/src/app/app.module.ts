fafaa77084fe8857d4574a65f7b60320485c5a25
Bastienlsg <bastien.lassaigne@gmail.com>
2023-08-27 18:57:33 +0200

    feat: add 404 page error

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
   ],
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   bootstrap: [
     AppComponent,
